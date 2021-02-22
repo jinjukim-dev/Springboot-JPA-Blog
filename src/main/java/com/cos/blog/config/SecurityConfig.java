e1d580c509cf1e18e07467aa319288417f328630
jinjukim-dev <kimjinju.dev@gmail.com>
2021-02-23 01:15:31 +0900

    add Spring Security login, join

---
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.cos.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.cos.blog.config.auth.PrincipalDetailService;
+
+import ch.qos.logback.core.pattern.color.BoldCyanCompositeConverter;
+
+// 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
+@Configuration // 빈등록 (IoC 관리)
+@EnableWebSecurity // 시큐리티 필터가 등록이 된다.
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻.
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private PrincipalDetailService principalDetailService;
+
+	@Bean // IoC 가 됨.
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// 시큐리티가 대신 로그인해주는데 password를 가로채기를 하는데
+	// 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야
+	// 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있다.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable() // csrf 토큰 비활성화 (테스트 시 걸우두는게 좋음)
+			.authorizeRequests() // 인증이 필요한 요청
+				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+			.and()
+				.formLogin()
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc")
+				.defaultSuccessUrl("/"); // 스프링 시큐리티가 해당 주소로 오는 로그인을 가로채서 대신 로그인 해준다.
+	}
+}
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -8,6 +11,15 @@ import com.cos.blog.model.User;
 // 자동으로 bean 등록이 된다
 // @Repository 어노테이션 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer>{ // User 테이블의 PK Integer이다.
-	
-	
+
+	// SELECT * FROM user WEHERE username = 1?;
+	Optional<User> findByUsername(String username);
 }
+
+
+// JPA Naming 쿼리
+// SELECT * FROM user WHERE username = ?1 AND password = ?2;
+//User findByUsernameAndPassword(String username, String password);
+
+//	@Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2", nativeQuery = true)
+//	User login(String username, String password);
